a7464e2313096fa752da9f780fcf517aa6068eaf
halac123b <halac123b@gmail.com>
2025-01-09 20:04:57 +0700

    Add m_1

---
+++ 1/main.py
@@ -0,0 +1,8 @@
+import numpy as np
+import sklearn.preprocessing
+
+input_data = np.array([2.1, -1.9, 5.5],
+                      [-1.5, 2.4, 3.5],
+                      [0.5, -7.9, 5.6],
+                      [5.9, 2.3, -5.8])
+
